ceec7e9894d6843a7231b61401e55e3dd0554fd8
Users <Usey>
2019-05-10 13:41:07 +0300

    asf

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,7 +19,7 @@
                 Single Page Application
             </p>
             <p>
-                MASTER for live master
+                MASTER for server
             </p>
         </div>
     </div>
