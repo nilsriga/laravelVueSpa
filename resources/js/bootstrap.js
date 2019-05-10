9e3ef0d8bc14ee21f9193ec30b5e81ba4cddf6c1
Users <Usey>
2019-05-10 16:07:48 +0300

    window wide socket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import form from './utilities/Form';
 
 
-window.socket = io('https://192.168.10.10:3000');
+window.socket = io('https://beta.spa-laravel-5-8:3000');
 window.Vue = Vue;
 window.axios = axios;
 window.Form = form;
