d7932db14a01d37a7c23cc03f9ebd76defa46109
Users <Usey>
2019-05-12 13:59:31 +0300

    window wide socket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router';
 import axios from 'axios';
 import form from './utilities/Form';
 
-window.socket = io('127.0.0.1:6379');
+window.socket = io('127.0.0.1:3000');
 window.Vue = Vue;
 window.axios = axios;
 window.Form = form;
